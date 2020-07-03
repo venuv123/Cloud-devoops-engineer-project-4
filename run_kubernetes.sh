87e63b6aaed66496a67e68c6c06c0d073f66b0e1
Venu v <39935490+venuv123@users.noreply.github.com>
2020-07-03 06:46:15 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="maxgherman/udacity-devops"
+dockerpath="venuv123/Cloud-devoops-engineer-project-5" 
 
 # Step 2
 # Run the Docker Hub container with kubernetes
